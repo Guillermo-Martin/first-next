82963ab12eb3b968e6d76ea3928e6bfc1dda80b2
gcmartin <gscalica@gmail.com>
2021-04-26 16:11:07 -0700

    first commit and create Next application

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+const Index = () => {
+  return (
+    <div>
+      <h1>Our Index Page</h1>
+    </div>
+  );
+}
+
+// or without curly braces (equivalent to above)
+// const Index = () => (
+//   <div>
+//     <h1>Our Index Page</h1>
+//   </div>
+// );
+
+export default Index;
