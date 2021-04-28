2ae5bc795882807835b36bba3c548c09848b95bf
gcmartin <gscalica@gmail.com>
2021-04-28 15:16:35 -0700

    Import 'withRouter' for another example of retrieving a query string

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bb582974fb75b47686da";
+/******/ 	var hotCurrentHash = "c6359732bc1699230b6d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/post.js
+++ pages/post.js
@@ -1,15 +1,18 @@
+import { withRouter } from "next/router";
+
 const Post = (props) => {
   return (
-    <h1>You're looking at post #{props.id}</h1>
+    <h1>You're looking at post #{props.router.query.id}</h1>
   );
 }
 
+// ===== getting the id from the query string using getInitialProps =====
 // getInitialProps receives and object (it can be called anything)
 // what's standard, though, is to grab the pieces you need
-Post.getInitialProps = async ({ query }) => {
-  // we now have access to "id" inside of props
-  return {id: query.id};
-}
+// Post.getInitialProps = async ({ query }) => {
+//   // we now have access to "id" inside of props
+//   return {id: query.id};
+// }
 
 
-export default Post;
+export default withRouter(Post);
