cdf1fcb6605f84a882537a5e04255af927dc146b
gcmartin <gscalica@gmail.com>
2021-04-28 15:50:57 -0700

    Add axios call to get comments and render comments

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c6359732bc1699230b6d";
+/******/ 	var hotCurrentHash = "4b88a29820b06d5653d0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/post.js
+++ pages/post.js
@@ -1,18 +1,49 @@
-import { withRouter } from "next/router";
+import axios from "axios";
 
-const Post = (props) => {
+const Post = ({ id, comments }) => {
+  const allComments = comments.map(comment => (
+    <Comment {...comment} key={comment.id}/>
+  ));
+
   return (
-    <h1>You're looking at post #{props.router.query.id}</h1>
+    <div>
+      <h1>Comments for post #{id}</h1>
+      {allComments}
+    </div>
+    
   );
+}
+
+const Comment = ({email, body}) => {
+   return (
+     <div>
+       <h5>{email}</h5>
+       <p>{body}</p>
+     </div>
+   )
 }
 
 // ===== getting the id from the query string using getInitialProps =====
 // getInitialProps receives and object (it can be called anything)
 // what's standard, though, is to grab the pieces you need
-// Post.getInitialProps = async ({ query }) => {
-//   // we now have access to "id" inside of props
-//   return {id: query.id};
+Post.getInitialProps = async ({ query }) => {
+  const response = await axios.get(`https://jsonplaceholder.typicode.com/comments?postId=${query.id}`);
+  const { data } = response;
+
+  // we now have access to "id" and comments inside of props
+  return {id: query.id, comments: data};
+}
+
+export default Post;
+
+
+// ===== retrieving ID using withRouter =====
+// import { withRouter } from "next/router";
+
+// const Post = (props) => {
+//   return (
+//     <h1>You're looking at post #{props.router.query.id}</h1>
+//   );
 // }
 
-
-export default withRouter(Post);
+// export default withRouter(Post);
